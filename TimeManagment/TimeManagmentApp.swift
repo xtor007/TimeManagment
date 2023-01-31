a771c81c86ade38fb32884838f3f3deeb7b968ca
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-01-31 20:42:39 +0200

    Initial Commit

---
+++ TimeManagment/TimeManagmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeManagmentApp.swift
+//  TimeManagment
+//
+//  Created by Anatoliy Khramchenko on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeManagmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
