e7768c195df4b31cb58942a2fd896c2100af7146
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-02-02 21:17:48 +0200

    Create model

---
+++ TimeManagment/Model/Event.swift
@@ -0,0 +1,13 @@
+//
+//  Event.swift
+//  TimeManagment
+//
+//  Created by Anatoliy Khramchenko on 02.02.2023.
+//
+
+import Foundation
+
+struct Event {
+    let title: String
+    let type: EventType
+}
