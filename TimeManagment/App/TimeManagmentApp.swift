5f28a6f9766761116561f86eca9cb8ea2bb87a7c
berlinsky <berlinsky.yar@gmail.com>
2023-02-03 04:35:02 +0200

    views with auth on firebase

--- TimeManagment/App/TimeManagmentApp.swift
+++ TimeManagment/App/TimeManagmentApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct TimeManagmentApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ TimeManagment/Extensions/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  TimeManagment
+//
+//  Created by Берлинский Ярослав Владленович on 03.02.2023.
+//
+
+import Foundation
+
+extension String {
+    func isValidEmail() -> Bool {
+        // test@email.com -> true
+        // test.com -> false
+        let regex = try? NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        return regex!.firstMatch(in: self, range: NSRange(location: 0, length: count)) != nil
+    }
+}
